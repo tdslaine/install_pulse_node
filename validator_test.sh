4b4115327130b97b92814c41008727f05217e377
tdslaine <46573429+tdslaine@users.noreply.github.com>
2023-04-03 10:07:43 +0200

    Update validator_test.sh
    added restore/backup option for validator_keys
    added graffiti user input/ default-grafiti with random number is used  if no input is given.
    added instruction at the end of the script, so User gets feedback.
    
    wip - check if the software is already installed prior to install it again to skip the process, save some time.
    wip - link to articles/tutorials for hardening/securing the node via fail2ban and ufw - general security best practice.
    wip - chang the useradd to --system, so it generates a system user instead of a normal user (gotta test this prior)

--- validator_test.sh
+++ validator_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # Add the deadsnakes PPA repository to install the latest Python version
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 
@@ -81,28 +85,86 @@ sudo python3 setup.py install
 
 # Run the helper script for installation
 #./deposit.sh install
+echo ""
+# Ask the user if they have previously created a validator_key
+echo "Have you previously created a validator_key and want to use these instead of a new one? (y/n)"
+read has_previous_key
+
+if [[ "$has_previous_key" =~ ^[Yy]$ ]]; then
+    # Ask the user to provide the backup path or use the default "/backup"
+    echo "Please enter the path to your validator_key backup (default: /backup):"
+    read backup_path
+
+    # Set the default value for backup path if the user enters nothing
+    if [ -z "$backup_path" ]; then
+        backup_path="/backup"
+    fi
+
+    # Restore the validator_keys from the backup
+    sudo cp -R "${backup_path}/validator_keys" "${custompath}/validator_keys"
+
+else
+    # Run the deposit.sh script with the entered fee-receiption address
+    echo "Now generating the validator keys - please follow the instructions and make sure to READ! everything"
+    sudo ./deposit.sh new-mnemonic --mnemonic_language=english --chain=pulsechain-testnet-v3 --folder="${custompath}"
+    cd "${custompath}"
+
+echo ""
+echo "please upload your generated "deposit_data-xxxyyyzzzz.json" to the validator dashboard at https://launchpad.v3.testnet.pulsechain.com; the deposit page is after client installation."
+#echo "now sleeping for 10"
+sleep 5
+echo ""
+
+    # Ask the user if they want to create a backup of the validator_keys
+    echo "Do you want to create a backup of your validator_keys? (y/n)"
+    read create_backup
+
+    if [[ "$create_backup" =~ ^[Yy]$ ]]; then
+        # Ask the user to provide the backup path or use the default "/backup"
+        echo "Please enter the path to save your validator_key backup (default: /backup):"
+        read backup_path
+
+        # Set the default value for backup path if the user enters nothing
+        if [ -z "$backup_path" ]; then
+            backup_path="/backup"
+        fi
+
+        # Create the backup directory if it doesn't exist
+        sudo mkdir -p "${backup_path}"
+
+        # Backup the validator_keys to the specified path
+        sudo cp -R "${custompath}/validator_keys" "${backup_path}/validator_keys"
+    fi
+fi
+
 
 # Ask the user to enter the fee-receiption address
-echo "Please enter the fee-receiption address (Press Enter to use the default address, which is mine lol):"
+echo ""
+echo "Please enter the fee-receiption address (if none is entered, my adress will be used.):"
 read fee_wallet
 
 # Use a regex pattern to validate the input wallet address
 if [[ -z "${fee_wallet}" ]] || ! [[ "${fee_wallet}" =~ ^0x[a-fA-F0-9]{40}$ ]]; then
-    fee_wallet="0x998D0ed46B837fbeAEb6988A6C00b721E33224Ec"
+    fee_wallet="0xCB00d822323B6f38d13A1f951d7e31D9dfDED4AA"
     echo "Using default fee-receiption address: ${fee_wallet}"
 else
     echo "Using provided fee-receiption address: ${fee_wallet}"
 fi
 
+# Generate a random number between 1000 and 9999
+random_number=$(shuf -i 1000-9999 -n 1)
 
-# Run the deposit.sh script with the entered fee-receiption address
-echo "now generating the validator keys - please follow the instructions and make sure to READ! everything"
-sudo ./deposit.sh new-mnemonic --num_validators=1 --mnemonic_language=english --chain=pulsechain-testnet-v3 --folder="${custompath}"
-cd "${custompath}"
+# Ask the user to enter their desired graffiti
+echo "Please enter your desired graffiti (default: HexForLife_${random_number}):"
+read user_graffiti
 
-echo "please upload your generated "deposit_data-xxxyyyzzzz.json" to the validator dashboard at https://launchpad.v3.testnet.pulsechain.com; the deposit page is after client installation."
-#echo "now sleeping for 10"
-#sleep 10
+# Set the default value for graffiti if the user enters nothing
+if [ -z "$user_graffiti" ]; then
+    user_graffiti="HexForLife_${random_number}"
+fi
+
+echo "Using graffiti: ${user_graffiti}"
+
 
 echo "importing keys using lighthouse"
 
@@ -130,6 +192,7 @@ VALIDATOR_LH="sudo -u validator docker run -it --network=host \\
     --network=pulsechain_testnet_v3 \\
     --validators-dir=/blockchain/validators \\
     --suggested-fee-recipient=${fee_wallet} \\
+	--graffiti='${user_graffiti}' \\
     --beacon-nodes=http://127.0.0.1:5052 "
 
 # Use a heredoc to create the start_validator_lh.sh file
@@ -142,3 +205,16 @@ sudo chmod +x start_validator_lh.sh
 
 # Change the ownership of the custompath/validator directory to validator user and group
 sudo chown -R validator:validator "${custompath}"
+
+echo -e "${GREEN} - Congratulations! The Node+Validator setup is now complete. You can start your execution and consensus clients to begin syncing with the Pulse chain.${NC}"
+echo ""
+echo -e "${GREEN} - To start the clients, navigate to the directory where the scripts were generated by running cd \"$custompath\" (default is cd /blockchain) in your terminal.${NC}"
+echo -e "${GREEN} - To run a script, use ./SCRIPTNAME (replace SCRIPTNAME with the actual name of the script you want to run).${NC}"
+echo -e "${GREEN} - If needed, you can change options/flags inside the corresponding .sh script(s).${NC}"
+echo -e "${GREEN} - Start the execution client first, followed by the consensus client, and finally the validator client.${NC}"
+echo ""
+echo -e " - Please ensure the chain is fully synced before starting your validator. Do not use the same keys on different machines simultaneously."
+echo ""
+echo " - In case of errors, check if there are any docker images already running by using the command \"sudo docker ps\". If there are, you can stop them using the \"sudo docker stop ID-NUMBER or NAME\" command."
+echo " - You might also need to prune the container using the \"sudo docker container prune\" command."
+
