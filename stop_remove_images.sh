90c6be38ced484f4983583e3790faeb35d5cbf14
tdslaine <46573429+tdslaine@users.noreply.github.com>
2023-04-06 12:21:43 +0200

    Create stop_remove_images.sh

---
+++ stop_remove_images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo docker stop execution
+sudo docker stop beacon
+sudo docker stop validator
+
+sudo docker rm execution
+sudo docker rm beacon
+sudo docker rm validator
+
+sudo docker container prune
