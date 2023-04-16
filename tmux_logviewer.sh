b2f5e00b6f65993cad0f58afff4a2ea38a9a40e1
tdslaine <46573429+tdslaine@users.noreply.github.com>
2023-04-16 23:54:24 +0200

    Create tmux_logviewer.sh
    only for advanced users.

---
+++ tmux_logviewer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create a new tmux session named 'logs' and run 'sudo docker logs -f execution' in it
+tmux new-session -d -s logs -n "Execution Logs" 'sudo docker logs -f execution'
+
+# Split the window horizontally and run 'sudo docker logs -f beacon'
+tmux split-window -h -t logs:0.0 'sudo docker logs -f beacon'
+
+# Select the first pane and split it vertically to run 'sudo docker logs -f validator'
+tmux split-window -v -t logs:0.0 'sudo docker logs -f validator'
+
+# Create a new window with 'htop' running
+tmux new-window -n "HTOP" -t logs:1 'htop'
+
+# Create another new window with 'sudo watch df -H' running
+tmux new-window -n "DiskUsage" -t logs:2 'sudo watch df -H'
+
+# Attach to the tmux session
+tmux attach-session -t logs
