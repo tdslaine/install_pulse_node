d706ac4cd3b1a9e47e6046ebc6c321387cf5d636
tdslaine <46573429+tdslaine@users.noreply.github.com>
2023-04-22 08:40:33 +0200

    Update tmux_logviewer.sh
    udpated split view to 4 windows panes

--- tmux_logviewer.sh
+++ tmux_logviewer.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 # Create a new tmux session named 'logs' and run 'sudo docker logs -f execution' in it
-tmux new-session -d -s logs -n "Logs" 'sudo docker logs -f execution'
+tmux new-session -d -s logs 'sudo docker logs -f execution'
 
 # Split the window horizontally and run 'sudo docker logs -f beacon'
-tmux split-window -h -t logs:0.0 'sudo docker logs -f beacon'
+tmux split-window -h -t logs 'sudo docker logs -f beacon'
 
 # Select the first pane and split it vertically to run 'sudo docker logs -f validator'
 tmux split-window -v -t logs:0.0 'sudo docker logs -f validator'
 
-# Create a new window with 'htop' running
-tmux new-window -n "HTOP" -t logs:1 'htop'
+# Select the second pane (beacon) and split it vertically to run 'htop'
+tmux split-window -v -t logs:0.1 'htop'
 
-# Create another new window with 'sudo watch df -H' running
-tmux new-window -n "DiskUsage" -t logs:2 'sudo watch df -H'
+# Evenly distribute the pane sizes
+tmux select-layout -t logs even-horizontal
+
+# Create a new window with 'sudo watch df -H' running
+tmux new-window -n "DiskUsage" -t logs:1 'sudo watch df -H'
 
 # Attach to the tmux session
 tmux attach-session -t logs:0.0
+
