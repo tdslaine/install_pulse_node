8116b20d61485f8e458c137c9387e0b541ede8f4
tdslaine <46573429+tdslaine@users.noreply.github.com>
2023-04-24 11:33:39 +0200

    Update log_viewer.sh
    removed sudo  to run the logs

--- log_viewer.sh
+++ log_viewer.sh
@@ -3,16 +3,16 @@
 # Open three new terminal windows and execute the specified commands in each
 
 # Open the first terminal window and execute the first command
-gnome-terminal --tab --title="Execution Logs" --command="bash -c 'sudo docker logs -f execution'"
+gnome-terminal --tab --title="Execution Logs" --command="bash -c 'docker logs -f execution'"
 
 # Open the second terminal window and execute the second command
-gnome-terminal --tab --title="Beacon Logs" --command="bash -c 'sudo docker logs -f beacon'"
+gnome-terminal --tab --title="Beacon Logs" --command="bash -c 'docker logs -f beacon'"
 
 # Open the third terminal window and execute the third command
-gnome-terminal --tab --title="Validator Logs" --command="bash -c 'sudo docker logs -f validator'"
+gnome-terminal --tab --title="Validator Logs" --command="bash -c 'docker logs -f validator'"
 
 # Open the fourth terminal window and execute htop
 gnome-terminal --tab --title="HTOP" --command="bash -c 'htop'"
 
 # Open the fifth terminal window and execute df -h
-gnome-terminal --tab --title="DiskUsage" --command="bash -c 'sudo watch df -H'"
+gnome-terminal --tab --title="DiskUsage" --command="bash -c 'watch df -H'"
