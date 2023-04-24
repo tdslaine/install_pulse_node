f9b0ac1a9548f705f660eba1844e842b7e1f62f4
tdslaine <46573429+tdslaine@users.noreply.github.com>
2023-04-24 12:29:15 +0200

    Update log_viewer.sh

--- log_viewer.sh
+++ log_viewer.sh
@@ -3,16 +3,16 @@
 # Open three new terminal windows and execute the specified commands in each
 
 # Open the first terminal window and execute the first command
-gnome-terminal --tab --title="Execution Logs" --command="bash -c 'docker logs -f execution'"
+gnome-terminal --tab --title="Execution Logs" -- bash -c "docker logs -f execution; exec bash"
 
 # Open the second terminal window and execute the second command
-gnome-terminal --tab --title="Beacon Logs" --command="bash -c 'docker logs -f beacon'"
+gnome-terminal --tab --title="Beacon Logs" -- bash -c "docker logs -f beacon; exec bash"
 
 # Open the third terminal window and execute the third command
-gnome-terminal --tab --title="Validator Logs" --command="bash -c 'docker logs -f validator'"
+gnome-terminal --tab --title="Validator Logs" -- bash -c "docker logs -f validator; exec bash"
 
 # Open the fourth terminal window and execute htop
-gnome-terminal --tab --title="HTOP" --command="bash -c 'htop'"
+gnome-terminal --tab --title="HTOP" -- bash -c "htop; exec bash"
 
 # Open the fifth terminal window and execute df -h
-gnome-terminal --tab --title="DiskUsage" --command="bash -c 'watch df -H'"
+gnome-terminal --tab --title="DiskUsage" -- bash -c "watch df -H; exec bash"
