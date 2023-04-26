a32fac059dd92a88b1f3871411025224ae6a9ee3
tdslaine <46573429+tdslaine@users.noreply.github.com>
2023-04-26 22:19:24 +0200

    Update tmux_logviewer.sh

--- tmux_logviewer.sh
+++ tmux_logviewer.sh
@@ -16,13 +16,13 @@ tmux split-window -v -t logs:0.1
 tmux select-layout -t logs tiled
 
 # Send the commands to the respective panes
-tmux send-keys -t logs:0.0 'sudo docker logs -f execution' Enter
-tmux send-keys -t logs:0.1 'sudo docker logs -f validator' Enter
-tmux send-keys -t logs:0.2 'sudo docker logs -f beacon' Enter
+tmux send-keys -t logs:0.0 'docker logs -f execution' Enter
+tmux send-keys -t logs:0.1 'docker logs -f validator' Enter
+tmux send-keys -t logs:0.2 'docker logs -f beacon' Enter
 tmux send-keys -t logs:0.3 'htop' Enter
 
 # Create a new window with 'sudo watch df -H' running
-tmux new-window -n "DiskUsage" -t logs:1 'sudo watch df -H'
+tmux new-window -n "DiskUsage" -t logs:1 'watch df -H'
 
 # Select the first pane (0.0)
 tmux select-pane -t logs:0.0
