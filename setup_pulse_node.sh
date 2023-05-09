ab7c3653679ce03d3699bd0044bfe03486ae5388
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-05-09 09:53:54 +0200

    Update setup_pulse_node.sh

--- setup_pulse_node.sh
+++ setup_pulse_node.sh
@@ -496,6 +496,7 @@ sudo cp setup_validator.sh "$CUSTOM_PATH/helper"
 sudo cp setup_monitoring.sh "$CUSTOM_PATH/helper"
 sudo cp functions.sh "$CUSTOM_PATH/helper"
 sudo cp helper/LogoVector.svg "$CUSTOM_PATH/helper"
+sudo cp helper/bls_to_execution.sh "$CUSTOM_PATH/helper"
 
 
 # Permissions to folders
