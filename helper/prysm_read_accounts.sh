8bc293876e6544e57ca4d99a100ec464bc1e0901
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-05-09 21:47:28 +0200

    Create prysm_read_accounts.sh
    addded prysm specific - read account script

---
+++ helper/prysm_read_accounts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source functions.sh
+
+# Prompt the user to enter the installation path
+read -e -p "Enter the installation path (default: /blockchain): " INSTALL_PATH
+
+# Set the default installation path if the user didn't enter a value
+if [ -z "$INSTALL_PATH" ]; then
+    INSTALL_PATH="/blockchain"
+fi
+
+# Run the Docker command with the specified installation path
+docker run --rm -it \
+--name="read_keys" \
+-v "${INSTALL_PATH}/wallet:/wallet" \
+registry.gitlab.com/pulsechaincom/prysm-pulse/validator:latest \
+accounts list \
+--${PRYSM_NETWORK_FLAG} \
+--wallet-dir=/wallet --wallet-password-file=/wallet/pw.txt
+
+read -p "Press Enter to continue..."
