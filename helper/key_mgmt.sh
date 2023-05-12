5df36b994686c44b4cc4f4eb8878b6119c29deee
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-05-12 22:04:22 +0200

    Create key_mgmt.sh
    added key-mgmt for the validator-menu - seperated from exit-validator and seperate from initial setup.

---
+++ helper/key_mgmt.sh
@@ -0,0 +1,336 @@
+#!/bin/bash
+
+start_dir=$(pwd)
+script_dir=$(dirname "$0")
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+source "$script_dir/functions.sh"
+
+get_main_user
+tab_autocomplete
+get_install_path
+
+function get_user_choices() {
+    echo "-----------------------------------------"
+    echo "       Choose your Validator Client      "
+    echo "-----------------------------------------"
+    echo "(based on your consensus/beacon Client)"
+    echo ""
+    echo "1. Lighthouse"
+    echo "2. Prysm"
+    echo ""
+    echo "0. Return or Exit"
+    echo ""
+    read -p "Enter your choice (1, 2 or 0): " client_choice
+
+    # Validate user input for client choice
+    while [[ ! "$client_choice" =~ ^[0-2]$ ]]; do
+        echo "Invalid input. Please enter a valid choice (1, 2 or 0): "
+        read -p "Enter your choice (1, 2 or 0): " client_choice
+    done
+
+    if [[ "$client_choice" == "0" ]]; then
+        echo "Exiting..."
+        exit 0
+    fi
+}
+get_user_choices
+
+
+
+generate_new_validator_key() {
+
+    if [[ "$client_choice" == "1" ]]; then
+        check_and_pull_lighthouse
+    elif [[ "$client_choice" == "2" ]]; then
+        check_and_pull_prysm_validator
+    fi
+
+    echo "Adding into an existing setup requires all running validator-clients to stop. This action will take place now."
+    press_enter_to_continue
+    stop_docker_container "validator" >/dev/null 2>&1
+
+    warn_network
+    clear
+
+    if [[ "$network_off" =~ ^[Yy]$ ]]; then
+        network_interface_DOWN
+    fi
+
+    echo ""
+    echo "Generating the validator keys via staking-cli"
+    echo ""
+    echo "Please follow the instructions and make sure to READ! and understand everything on screen"
+    echo ""
+    echo -e "${RED}Attention:${NC}"
+    echo ""
+    echo "The next step requires you to enter the wallet address that you would like to use for receiving"
+    echo "validator rewards while validating and withdrawing your funds when you exit the validator pool."
+    echo -e "This is the ${GREEN}Withdrawal- or Execution-Wallet (they are the same)${NC}"
+    echo ""
+    echo -e "Make sure ${RED}you have full access${NC} to this Wallet. ${RED}Once set, it cannot be changed${NC}"
+    echo ""
+    echo -e "You need to provide this Wallet-Adresss in the ${GREEN}proper format (checksum)${NC}."
+    echo -e "One way to achive this, is to copy your adress from the Blockexplorer"
+    echo ""
+    if confirm_prompt "I have read this information and confirm that I understand the importance of using the right Withdrawal-Wallet Address."; then
+        echo ""
+        echo "proceeding..."
+        sleep 2
+    else
+        echo "Exiting script now."
+        network_interface_UP
+        exit 1
+    fi
+
+
+    echo ""
+
+# Check if the address is a valid address, loop until it is...
+while true; do
+    read -e -p "Please enter your Execution/Withdrawal-Wallet address: " withdrawal_wallet
+    if [[ "${withdrawal_wallet}" =~ ^0x[a-fA-F0-9]{40}$ ]]; then
+        break
+    else
+        echo "Invalid address format. Please enter a valid PRC20 address."
+    fi
+done
+
+    
+    # Running staking-cli to Generate the new validator_keys
+    echo ""
+    echo "Starting staking-cli to Generate the new validator_keys"
+    echo ""
+
+    ${INSTALL_PATH}/staking-deposit-cli/deposit.sh new-mnemonic \
+    --mnemonic_language=english \
+    --chain=${DEPOSIT_CLI_NETWORK} \
+    --folder="${INSTALL_PATH}" \
+    --eth1_withdrawal_address="${withdrawal_wallet}"
+
+
+    cd "${INSTALL_PATH}"
+    sudo chmod -R 770 "${INSTALL_PATH}/validator_keys"
+    sudo chmod -R 770 "${INSTALL_PATH}/wallet"
+
+
+    if [[ "$network_off" =~ ^[Yy]$ ]]; then
+        network_interface_UP
+    fi
+
+    if [[ "$client_choice" == "1" ]]; then
+        import_lighthouse_validator
+    elif [[ "$client_choice" == "2" ]]; then
+        import_prysm_validator
+    fi
+
+sudo find "$INSTALL_PATH/validator_keys" -type f -name "keystore*.json" -exec sudo chmod 440 {} \;
+sudo find "$INSTALL_PATH/validator_keys" -type f -name "deposit*.json" -exec sudo chmod 444 {} \;
+sudo find "$INSTALL_PATH/validator_keys" -type f -exec sudo chown $main_user:pls-validator {} \;
+
+
+
+    start_script ../start_validator
+    
+    echo ""
+    sudo chmod -R 440 "${INSTALL_PATH}/validator_keys"
+    echo "Import into existing Setup done."
+    restart_tmux_logs_session
+    exit 0
+    
+}
+
+################################################### Import ##################################################
+import_restore_validator_keys() {
+
+    if [[ "$client_choice" == "1" ]]; then
+        check_and_pull_lighthouse
+    elif [[ "$client_choice" == "2" ]]; then
+        check_and_pull_prysm_validator
+    fi
+
+    echo "Importing into an existing setup requires all running validator-clients to stop. This action will take place now."
+    press_enter_to_continue
+    stop_docker_container "validator" >/dev/null 2>&1
+
+
+
+    while true; do
+        clear
+        # Prompt the user to enter the path to the root directory containing the 'validator_keys' backup-folder
+        echo -e "Enter the path to the root directory which contains the 'validator_keys' backup-folder."
+        echo -e "For example, if your 'validator_keys' folder is located in '/home/user/my_backup/validator_keys',"
+        echo -e "then provide the path '/home/user/my_backup'. You can use tab-autocomplete when entering the path."
+        echo ""
+        read -e -p "Path to backup: " backup_path
+    
+        # Check if the source directory exists
+        if [ -d "${backup_path}/validator_keys" ]; then
+            # Check if the source and destination paths are different
+            if [ "${INSTALL_PATH}/validator_keys" != "${backup_path}/validator_keys" ]; then
+                # Copy the validator_keys folder to the install path
+                sudo cp -R "${backup_path}/validator_keys" "${INSTALL_PATH}"
+                # Inform the user that the keys have been successfully copied over
+                echo "Keys successfully copied."
+                # Exit the loop
+                break
+            else
+                # Inform the user that the source and destination paths match and no action is needed
+                echo "Source and destination paths match. Skipping restore-copy; keys seem already in place."
+                echo "Key import will still proceed..."
+                # Exit the loop
+                break
+            fi
+        else
+            # Inform the user that the source directory does not exist and ask them to try again
+            echo "Source directory does not exist. Please check the provided path and try again."
+        fi
+    done
+    
+        
+    echo ""
+    echo "Importing validator keys now"
+    echo ""
+
+    sudo chmod -R 770 "${INSTALL_PATH}/validator_keys"
+    sudo chmod -R 770 "${INSTALL_PATH}/wallet"
+    
+    if [[ "$client_choice" == "1" ]]; then
+        import_lighthouse_validator
+        elif [[ "$client_choice" == "2" ]]; then
+        import_prysm_validator
+    fi
+
+sudo find "$INSTALL_PATH/validator_keys" -type f -name "keystore*.json" -exec sudo chmod 440 {} \;
+sudo find "$INSTALL_PATH/validator_keys" -type f -name "deposit*.json" -exec sudo chmod 444 {} \;
+sudo find "$INSTALL_PATH/validator_keys" -type f -exec sudo chown $main_user:pls-validator {} \;
+
+
+     
+    start_script ../start_validator
+    
+    echo ""
+    #sudo chmod 550 "${INSTALL_PATH}/validator_keys"
+    echo "Import into existing Setup done."
+    restart_tmux_logs_session
+    exit 0
+            
+}
+
+################################################### Restore ##################################################
+# Function to restore from SeedPhrase 
+Restore_from_MN() {
+
+    echo "Restoring validator_keys from SeedPhrase (Mnemonic)"
+
+    if [[ "$client_choice" == "1" ]]; then
+        check_and_pull_lighthouse
+    elif [[ "$client_choice" == "2" ]]; then
+        check_and_pull_prysm_validator
+    fi
+
+    echo "Importing into an existing setup requires all running validator-clients to stop. This action will take place now."
+    press_enter_to_continue
+    stop_docker_container "validator" >/dev/null 2>&1
+    
+    clear
+
+    warn_network
+
+    clear
+
+
+    if [[ "$network_off" =~ ^[Yy]$ ]]; then
+        network_interface_DOWN
+    fi
+    # Check if the address is a valid address, loop until it is...
+    while true; do
+    read -e -p "Please enter your Execution/Withdrawal-Wallet address: " withdrawal_wallet
+    if [[ "${withdrawal_wallet}" =~ ^0x[a-fA-F0-9]{40}$ ]]; then
+        break
+    else
+        echo "Invalid address format. Please enter a valid PRC20 address."
+    fi
+    done
+    
+    
+    echo ""
+    echo "Now running staking-cli command to restore from your SeedPhrase (Mnemonic)"
+    echo ""
+    
+    cd "${INSTALL_PATH}"
+    sudo chmod -R 777 "${INSTALL_PATH}/validator_keys"
+    sudo chmod -R 777 "${INSTALL_PATH}/wallet"
+       
+    cd ${INSTALL_PATH}/staking-deposit-cli/
+    ./deposit.sh existing-mnemonic \
+    --chain=${DEPOSIT_CLI_NETWORK} \
+    --folder="${INSTALL_PATH}" \
+    --eth1_withdrawal_address="${withdrawal_wallet}"
+     
+
+    if [[ "$network_off" =~ ^[Yy]$ ]]; then
+        network_interface_UP
+    fi
+
+    if [[ "$client_choice" == "1" ]]; then
+        import_lighthouse_validator
+    elif [[ "$client_choice" == "2" ]]; then
+        import_prysm_validator
+
+    fi
+
+sudo chmod -R 770 "${INSTALL_PATH}/validator_keys"
+sudo find "$INSTALL_PATH/validator_keys" -type f -name "keystore*.json" -exec sudo chmod 770 {} \;
+sudo find "$INSTALL_PATH/validator_keys" -type f -name "deposit*.json" -exec sudo chmod 774 {} \;
+sudo find "$INSTALL_PATH/validator_keys" -type f -exec sudo chown $main_user:pls-validator {} \;
+
+
+ 
+    start_script ../start_validator
+    
+    echo ""
+    echo "Import into existing Setup done."
+    #sudo chmod -R 770 "${INSTALL_PATH}/validator_keys"
+    restart_tmux_logs_session
+    exit 0
+}
+    
+
+
+# Selection menu
+echo "-----------------------------------------"
+echo "|           Validator Key Setup         |"
+echo "-----------------------------------------"
+echo ""
+PS3=$'\nChoose an option (1-4): '
+options=("Generate new validator_keys (fresh)" "Import/Restore validator_keys from a Folder (from Offline generation or Backup)" "Restore or add from a Seed Phrase (Mnemonic) " "Exit/Cancel")
+COLUMNS=1
+select opt in "${options[@]}"
+
+do
+    case $REPLY in
+        1)
+            generate_new_validator_key
+            break
+            ;;
+        2)
+            import_restore_validator_keys
+            break
+            ;;
+        3)
+            Restore_from_MN
+            break
+            ;;  
+
+        4)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid option. Please choose option (1-4)."
+            ;;
+    esac
+done
