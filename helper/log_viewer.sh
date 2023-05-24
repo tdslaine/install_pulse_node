c4189ed69f555e3b2286999313050068248a6c17
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-05-24 09:51:12 +0200

    Update log_viewer.sh
    silent termination of prev. running logs.
    added monitoring for running docker images via docker ps

--- helper/log_viewer.sh
+++ helper/log_viewer.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 # Close any existing terminal windows with the specified titles
-wmctrl -l | grep -w "Execution Logs\|Beacon Logs\|Validator Logs\|HTOP\|DiskUsage" | awk '{print $1}' | xargs -r wmctrl -ic
+wmctrl -l | grep -w "Execution Logs\|Beacon Logs\|Validator Logs\|HTOP\|DiskUsage" | awk '{print $1}' | xargs -r wmctrl -ic >/dev/null 2>&1
 
 # Wait for a moment to ensure the previous terminal windows are closed
 sleep 2
 
-# Open three new terminal windows and execute the specified commands in each
+# Open new terminal windows and execute the specified commands in each
 
-# Open the first terminal window and execute the first command
-gnome-terminal --tab --title="Execution Logs" -- bash -c "docker logs -f execution; exec bash"
+gnome-terminal --tab --title="Execution Logs" -- bash -c "docker logs -f --tail=20 execution; exec bash"
 
-# Open the second terminal window and execute the second command
-gnome-terminal --tab --title="Beacon Logs" -- bash -c "docker logs -f beacon; exec bash"
+gnome-terminal --tab --title="Beacon Logs" -- bash -c "docker logs -f --tail=20 beacon; exec bash"
 
-# Open the third terminal window and execute the third command
-gnome-terminal --tab --title="Validator Logs" -- bash -c "docker logs -f validator; exec bash"
+gnome-terminal --tab --title="Validator Logs" -- bash -c "docker logs -f --tail=20 validator; exec bash"
 
-# Open the fourth terminal window and execute htop
 gnome-terminal --tab --title="HTOP" -- bash -c "htop; exec bash"
 
-# Open the fifth terminal window and execute df -h
 gnome-terminal --tab --title="DiskUsage" -- bash -c "watch df -H; exec bash"
+
+gnome-terminal --tab --title="Docker Status" --bash -c "watch docker ps; exec bash"
