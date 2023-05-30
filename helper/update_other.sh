de69e1f064e25b08cf6a7b059c50e095c5fd3028
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-05-30 20:49:34 +0200

    Create update_other.sh
    just added geth -to this one - so others "guide" sourced folks can use it too.

---
+++ helper/update_other.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Pull the latest watchtower image
+echo "Pulling the latest watchtower image..."
+docker pull containrrr/watchtower
+
+# Launch watchtower container with the --run-once flag
+echo "Launching watchtower with the --run-once flag..."
+docker run \
+    -d \
+    --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    containrrr/watchtower \
+    --run-once \
+    --include-stopped \
+    --include-restarting \
+    --revive-stopped \
+    geth \
+    execution \
+    beacon \
+    validator
+
+echo "Watchtower run-once process has completed."
