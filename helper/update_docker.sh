e9a8cee110e5cbecba91747fb5181841ce532d1b
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-06-02 12:42:51 +0200

    Update update_docker.sh

--- helper/update_docker.sh
+++ helper/update_docker.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# Pull the latest watchtower image
-echo "Pulling the latest watchtower image..."
-docker pull containrrr/watchtower
-
-# Launch watchtower container with the --run-once flag
-echo "Launching watchtower with the --run-once flag..."
-docker run \
-    --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    containrrr/watchtower \
-    --cleanup \
-    --run-once \
-    --include-stopped \
-    --include-restarting \
-    --revive-stopped 
-
-echo "Watchtower run-once process has completed."
+    echo "Stopping Docker-images..."
+    docker stop -t 300 execution
+    docker stop -t 180 beacon
+    docker stop -t 180 validator
+    docker container prune -f && docker image prune -f
+    echo "Restarting docker images from script..."
+   
