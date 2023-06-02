22ec8aec66d176f0ea673d82bfb3bba65742e4e4
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-06-02 13:05:51 +0200

    Update update_docker.sh

--- helper/update_docker.sh
+++ helper/update_docker.sh
@@ -5,5 +5,12 @@
     docker stop -t 180 beacon
     docker stop -t 180 validator
     docker container prune -f && docker image prune -f
-    echo "Restarting docker images from script..."
+    echo "Removing Docker-images..."
+    docker rmi registry.gitlab.com/pulsechaincom/go-pulse
+    docker rmi registry.gitlab.com/pulsechaincom/erigon-pulse
+    docker rmi registry.gitlab.com/pulsechaincom/lighthouse-pulse 
+    docker rmi registry.gitlab.com/pulsechaincom/prysm-pulse/beacon-chain
+    docker rmi registry.gitlab.com/pulsechaincom/prysm-pulse/validator  
+    
+
    
