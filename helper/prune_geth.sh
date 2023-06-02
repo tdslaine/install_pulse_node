64028f2d0aa6898ff55f6035efa928a12e6f1789
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-06-02 16:46:55 +0200

    Create prune_geth.sh
    adding prunning for geth

---
+++ helper/prune_geth.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "WARNING: The execution-client will be stopped and pruning will begin. DO NOT interrupt the process."
+
+read -p "Would you like to continue? (y/n): " CONTINUE
+CONTINUE=${CONTINUE:-n}
+
+if [ "$CONTINUE" != "y" ]; then
+    echo "Exiting without making changes."
+    exit 1
+fi
+
+# Prompt the user for their install path, default to /blockchain if nothing is entered
+read -p "Enter the installation path [/blockchain]: " INSTALL_PATH
+INSTALL_PATH=${INSTALL_PATH:-/blockchain}
+INSTALL_PATH="${INSTALL_PATH%/}"
+
+echo "Using install path: $INSTALL_PATH"
+
+# Stop the execution-client and prune Docker
+sudo docker stop -t 300 execution && sudo docker container prune -f
+
+# Run the Docker command to prune the state
+sudo docker run --rm --name geth_prune -it -v $INSTALL_PATH/execution/geth:/geth \
+registry.gitlab.com/pulsechaincom/go-pulse:latest \
+snapshot prune-state \
+--datadir /geth
+
+# Start the execution-client again after pruning
+bash $INSTALL_PATH/start_execution.sh
+
+echo "Pruning is done and execution-client is resumed. Please check the status of your execution-client."
+
