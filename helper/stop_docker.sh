78c7691a52a649721b12412d3f0a2b67d8b99f52
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-06-02 12:48:44 +0200

    Update stop_docker.sh

--- helper/stop_docker.sh
+++ helper/stop_docker.sh
@@ -4,8 +4,4 @@ sudo docker stop -t 300 execution
 sudo docker stop -t 180 beacon
 sudo docker stop -t 180 validator
 
-sudo docker rm execution
-sudo docker rm beacon
-sudo docker rm validator
-
-sudo docker container prune -f && sudo docker image prune -f
+sudo docker container prune -f 
