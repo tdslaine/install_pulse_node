8be041e2ab9ac1a2ca05d7aecdafc5aae0f17835
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-06-02 13:15:26 +0200

    Update restart_docker.sh
    might be depracted - not needed anylonger, all done via stop and CUSTOM_PATH_script-launch function

--- helper/restart_docker.sh
+++ helper/restart_docker.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-#This is the main Folder, where your start_scripts reside, please change according to your setup (default /blockchain)
-CONFIG_FOLDER=/blockchain
 
 sudo docker stop -t 300 execution
 sudo docker stop -t 180 beacon
 sudo docker stop -t 180 validator
 
-sudo docker rm execution
-sudo docker rm beacon
-sudo docker rm validator
-
 sudo docker container prune -f
-
-sudo $CONFIG_FOLDER/start_execution.sh 
-sudo $CONFIG_FOLDER/start_consensus.sh
-sudo $CONFIG_FOLDER/start_validator.sh
