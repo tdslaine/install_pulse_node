83955361e76df0a939b6645b2003503a783d917e
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-06-04 10:22:15 +0200

    Update lighthouse_status_batch.sh
    added hardcoded and external fed .txt, added anoher cmd toncheck attestations health

--- helper/lighthouse_status_batch.sh
+++ helper/lighthouse_status_batch.sh
@@ -1,20 +1,55 @@
-#!/bin/bash
 
-# Define the file containing your validator indices
+#!/bin/bash 
+
+# Define your beacon node address 
+BEACON_NODE=http://localhost:5052 
+
+# Define the path to your file
 INDICES_FILE=/blockchain/valis.txt
 
-# Define your beacon node address
-BEACON_NODE=http://localhost:5052
+# Array of hardcoded validator indices
+HARDCODED_INDICES=(6694 6695 6696)
 
-# Loop through each line in the indices file
-while read -r VALIDATOR_INDEX
+# Infinite loop
+while true
 do
-  echo "Processing validator index $VALIDATOR_INDEX..."
+  # Read from external file if it exists
+  if [ -f "$INDICES_FILE" ]; then
+    while read -r VALIDATOR_INDEX 
+    do 
+      echo "Processing validator index $VALIDATOR_INDEX..." 
 
-  # Run the curl command for the current validator index
-  curl -s -X GET "$BEACON_NODE/eth/v1/beacon/states/head/validators/$VALIDATOR_INDEX" -H "accept: application/json" | jq -r '{index: .data.index, status: .data.status}'
+      # Run the GET curl command for the current validator index 
+      curl -s -X GET "$BEACON_NODE/eth/v1/beacon/states/head/validators/$VALIDATOR_INDEX" -H "accept: application/json" | jq -r '{index: .data.index, status: .data.status}'
 
-  echo "Finished processing validator index $VALIDATOR_INDEX."
-done < "$INDICES_FILE"
+      # Run the POST curl command for the current validator index 
+      curl -X POST "$BEACON_NODE/lighthouse/ui/validator_metrics" -d "{\"indices\": [$VALIDATOR_INDEX]}" -H "Content-Type: application/json" | jq 
 
-echo "All done!"
+      echo "Finished processing validator index $VALIDATOR_INDEX." 
+
+      # Sleep for 3 seconds
+      sleep 3
+    done < "$INDICES_FILE"
+  else
+    echo "File $INDICES_FILE not found!"
+  fi
+
+  # Process hardcoded indices
+  for VALIDATOR_INDEX in "${HARDCODED_INDICES[@]}"
+  do
+    echo "Processing validator index $VALIDATOR_INDEX..." 
+
+    # Run the GET curl command for the current validator index 
+    curl -s -X GET "$BEACON_NODE/eth/v1/beacon/states/head/validators/$VALIDATOR_INDEX" -H "accept: application/json" | jq -r '{index: .data.index, status: .data.status}'
+
+    # Run the POST curl command for the current validator index 
+    curl -X POST "$BEACON_NODE/lighthouse/ui/validator_metrics" -d "{\"indices\": [$VALIDATOR_INDEX]}" -H "Content-Type: application/json" | jq 
+
+    echo "Finished processing validator index $VALIDATOR_INDEX." 
+
+    # Sleep for 3 seconds
+    sleep 3
+  done
+
+  echo "Finished one cycle, starting another..."
+done
