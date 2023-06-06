0241a7571ac89e700757c9afabbda0c27a55b461
DipSlayer <46573429+tdslaine@users.noreply.github.com>
2023-06-06 21:40:25 +0200

    Update tmux_logviewer.sh
    tailing log output

--- helper/tmux_logviewer.sh
+++ helper/tmux_logviewer.sh
@@ -19,9 +19,9 @@ tmux split-window -v -t logs:0.1
 tmux select-layout -t logs tiled
 
 # Send the commands to the respective panes
-tmux send-keys -t logs:0.0 'docker logs -f execution' Enter
-tmux send-keys -t logs:0.1 'docker logs -f validator' Enter
-tmux send-keys -t logs:0.2 'docker logs -f beacon' Enter
+tmux send-keys -t logs:0.0 'docker logs -f --tail=30 execution' Enter
+tmux send-keys -t logs:0.1 'docker logs -f --tail=30 validator' Enter
+tmux send-keys -t logs:0.2 'docker logs -f --tail=30 beacon' Enter
 tmux send-keys -t logs:0.3 'htop' Enter
 
 # Create a new window with 'sudo watch df -H' running
